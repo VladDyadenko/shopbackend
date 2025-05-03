02b12607f08297714509bd905a515e34f1071e9d
Vladimir Dyadenko <Stilmagroup@gmail.com>
2025-05-03 18:57:39 +0300

    authorization completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import {
 	Body,
 	Controller,
+	Get,
 	HttpCode,
 	Post,
+	Req,
+	Res,
+	UnauthorizedException,
+	UseGuards,
 	UsePipes,
 	ValidationPipe
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { createUserDto } from '@user/dto/create-user.dto'
+import { Response, Request } from 'express'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
 export class AuthController {
@@ -16,14 +23,77 @@ export class AuthController {
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: createUserDto) {
-		return await this.authService.login(dto)
+	async login(
+		@Body() dto: createUserDto,
+		@Res({ passthrough: true }) res: Response
+	) {
+		const { refreshToken, ...response } = await this.authService.login(dto)
+		this.authService.addRefreshTokenToRespons(res, refreshToken)
+
+		return response
 	}
 
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('register')
-	async register(@Body() dto: createUserDto) {
-		return await this.authService.register(dto)
+	async register(
+		@Body() dto: createUserDto,
+		@Res({ passthrough: true }) res: Response
+	) {
+		const { refreshToken, ...response } =
+			await this.authService.register(dto)
+		this.authService.addRefreshTokenToRespons(res, refreshToken)
+
+		return response
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login/access-token')
+	async getNewTokens(
+		@Req() req: Request,
+		@Res({ passthrough: true }) res: Response
+	) {
+		const refreshTokenFromCookies =
+			req.cookies[this.authService.REFRESH_TOKEN_NAME]
+
+		if (!refreshTokenFromCookies) {
+			this.authService.removeRefreshTokenFromRespons(res)
+			throw new UnauthorizedException('Refresh токен не пройшов')
+		}
+		const { refreshToken, ...response } =
+			await this.authService.getNewTokens(refreshTokenFromCookies)
+
+		this.authService.addRefreshTokenToRespons(res, refreshToken)
+
+		return response
+	}
+
+	@HttpCode(200)
+	@Post('logout')
+	logout(@Res({ passthrough: true }) res: Response) {
+		this.authService.removeRefreshTokenFromRespons(res)
+		return true
+	}
+
+	@Get('google')
+	@UseGuards(AuthGuard('google'))
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async googleAuth(@Req() req) {}
+
+	@Get('google/callback')
+	@UseGuards(AuthGuard('google'))
+	async googleAuthCallback(
+		@Req() req,
+		@Res({ passthrough: true }) res: Response
+	) {
+		const { refreshToken, ...response } =
+			await this.authService.validateOAuthLogin(req)
+
+		this.authService.addRefreshTokenToRespons(res, refreshToken)
+
+		return res.redirect(
+			`${process.env['CLIENT_URL']}/dashboard?accessToken=${response.accessToken}`
+		)
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,26 @@
 import {
 	BadRequestException,
 	Injectable,
-	NotFoundException
+	NotFoundException,
+	UnauthorizedException
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { createUserDto } from '@user/dto/create-user.dto'
 import { UserService } from '@user/user.service'
+import { Response } from 'express'
 import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class AuthService {
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
+
 	constructor(
 		private jwt: JwtService,
 		private userService: UserService,
-		private prisma: PrismaService
+		private prisma: PrismaService,
+		private configService: ConfigService
 	) {}
 
 	async login(dto: createUserDto) {
@@ -29,6 +36,16 @@ export class AuthService {
 		if (oldUser) throw new BadRequestException('Користувач вже існує')
 
 		const user = await this.userService.create(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		if (!result) throw new UnauthorizedException('Невалідний токен')
+
+		const user = await this.userService.getById(result.id)
 		const tokens = this.issueTokens(user.id)
 
 		return { user, ...tokens }
@@ -52,5 +69,48 @@ export class AuthService {
 		}
 
 		return user
+	}
+
+	async validateOAuthLogin(req: any) {
+		let user = await this.userService.getByEmail(req.user.email)
+
+		if (!user) {
+			user = await this.prisma.user.create({
+				data: {
+					email: req.user.email,
+					name: req.user.name,
+					picture: req.user.picture
+				},
+				include: {
+					stores: true,
+					favorites: true,
+					orders: true
+				}
+			})
+		}
+		const tokens = this.issueTokens(user.id)
+		return { user, ...tokens }
+	}
+
+	addRefreshTokenToRespons(res: Response, refreshToken: string) {
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: this.configService.get('SERVER_DOMAIN'),
+			expires: expiresIn,
+			secure: true,
+			sameSite: 'none'
+		})
+	}
+	removeRefreshTokenFromRespons(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+			httpOnly: true,
+			domain: this.configService.get('SERVER_DOMAIN'),
+			expires: new Date(0),
+			secure: true,
+			sameSite: 'none'
+		})
 	}
 }
