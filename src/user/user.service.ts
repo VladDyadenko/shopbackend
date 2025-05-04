5c7b84853777bf4b9c94d894138b33c033aa9550
Vladimir Dyadenko <Stilmagroup@gmail.com>
2025-05-04 07:05:30 +0300

    add new folders, complit stor

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -31,6 +31,29 @@ export class UserService {
 		return user
 	}
 
+	async toggleFavorite(productId: string, userId: string) {
+		const user = await this.getById(userId)
+
+		const isExists = user.favorites.some(
+			product => product.id === productId
+		)
+
+		await this.prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				favorites: {
+					[isExists ? 'disconnect' : 'connect']: {
+						id: productId
+					}
+				}
+			}
+		})
+
+		return true
+	}
+
 	async create(dto: createUserDto) {
 		const user = await this.prisma.user.create({
 			data: {
