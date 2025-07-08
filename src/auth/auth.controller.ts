2b207895ac3eea18ec5f022828f2695a088c9bdf
Vladimir Dyadenko <dyadenkoweb@gmail.com>
2025-07-08 22:40:02 +0300

    change cookies settings

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,99 +1,99 @@
 import {
-	Body,
-	Controller,
-	Get,
-	HttpCode,
-	Post,
-	Req,
-	Res,
-	UnauthorizedException,
-	UseGuards,
-	UsePipes,
-	ValidationPipe
-} from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { createUserDto } from '@user/dto/create-user.dto'
-import { Response, Request } from 'express'
-import { AuthGuard } from '@nestjs/passport'
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { createUserDto } from '@user/dto/create-user.dto';
+import { Response, Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Post('login')
-	async login(
-		@Body() dto: createUserDto,
-		@Res({ passthrough: true }) res: Response
-	) {
-		const { refreshToken, ...response } = await this.authService.login(dto)
-		this.authService.addRefreshTokenToRespons(res, refreshToken)
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(
+    @Body() dto: createUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.login(dto);
+    this.authService.addRefreshTokenToRespons(res, refreshToken);
 
-		return response
-	}
+    return response;
+  }
 
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Post('register')
-	async register(
-		@Body() dto: createUserDto,
-		@Res({ passthrough: true }) res: Response
-	) {
-		const { refreshToken, ...response } =
-			await this.authService.register(dto)
-		this.authService.addRefreshTokenToRespons(res, refreshToken)
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(
+    @Body() dto: createUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.register(dto);
+    this.authService.addRefreshTokenToRespons(res, refreshToken);
 
-		return response
-	}
+    return response;
+  }
 
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Post('login/access-token')
-	async getNewTokens(
-		@Req() req: Request,
-		@Res({ passthrough: true }) res: Response
-	) {
-		const refreshTokenFromCookies =
-			req.cookies[this.authService.REFRESH_TOKEN_NAME]
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getNewTokens(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME];
 
-		if (!refreshTokenFromCookies) {
-			this.authService.removeRefreshTokenFromRespons(res)
-			throw new UnauthorizedException('Refresh токен не пройшов')
-		}
-		const { refreshToken, ...response } =
-			await this.authService.getNewTokens(refreshTokenFromCookies)
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenFromRespons(res);
+      throw new UnauthorizedException('Refresh токен не пройшов');
+    }
+    const { refreshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    );
 
-		this.authService.addRefreshTokenToRespons(res, refreshToken)
+    this.authService.addRefreshTokenToRespons(res, refreshToken);
 
-		return response
-	}
+    return response;
+  }
 
-	@HttpCode(200)
-	@Post('logout')
-	logout(@Res({ passthrough: true }) res: Response) {
-		this.authService.removeRefreshTokenFromRespons(res)
-		return true
-	}
+  @HttpCode(200)
+  @Post('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromRespons(res);
+    return true;
+  }
 
-	@Get('google')
-	@UseGuards(AuthGuard('google'))
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	async googleAuth(@Req() req) {}
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async googleAuth(@Req() req) {}
 
-	@Get('google/callback')
-	@UseGuards(AuthGuard('google'))
-	async googleAuthCallback(
-		@Req() req,
-		@Res({ passthrough: true }) res: Response
-	) {
-		const { refreshToken, ...response } =
-			await this.authService.validateOAuthLogin(req)
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthCallback(
+    @Req() req,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } =
+      await this.authService.validateOAuthLogin(req);
 
-		this.authService.addRefreshTokenToRespons(res, refreshToken)
+    this.authService.addRefreshTokenToRespons(res, refreshToken);
 
-		return res.redirect(
-			`${process.env['CLIENT_URL']}/dashboard?accessToken=${response.accessToken}`
-		)
-	}
+    return res.redirect(
+      `${process.env['CLIENT_URL']}/dashboard?accessToken=${response.accessToken}`,
+    );
+  }
 }
